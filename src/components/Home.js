97ffecc951a85b3577fc143e2257c3b36b3a888d
NataSerg <na.s@ukr.net>
2022-11-16 12:42:18 +0200

    frontend code added

---
+++ src/components/Home.js
@@ -0,0 +1,53 @@
+import "./Home.css";
+import { Col, Carousel } from "react-bootstrap";
+
+
+
+
+function Home() {
+  
+  return <> 
+      <div className="home-container d-flex flex-column justify-content-center">
+        <div className="first-words"><img className="home-image" src="/images/3.jpg" />BREATHE<img className="home-image" src="/images/1.jpg" /></div>
+        <div className="first-words"><img className="home-image" src="/images/13.jpg" /><img className="home-image" src="/images/12.jpg" />DRAW<img className="home-image" src="/images/22.jpg" /></div>
+        <div className="first-words"><img className="home-image" src="/images/2.jpg" />INSPIRE <img className="home-image" src="/images/24.jpg" /></div>
+         
+      <p>Creative drawings espessially for you</p>
+      <p>Beauty will save the world</p>
+      <p>The warmiest wishes for you through marvelous postcards</p>
+    <Carousel >
+  <Carousel.Item interval={3000}>
+    <img
+      className="d-block carousel-img"
+      src="/images/3.jpg"
+      alt="First slide"
+    />
+    
+  </Carousel.Item>
+  <Carousel.Item interval={3000}>
+    <img
+      className="d-block carousel-img"
+      src="/images/4.jpg"
+      alt="Second slide"
+    />
+
+    
+  </Carousel.Item>
+  <Carousel.Item interval={3000}>
+    <img
+      className="d-block carousel-img"
+      src="/images/5.jpg"
+      alt="Third slide"
+    />
+
+   
+  </Carousel.Item>
+</Carousel>
+      </div>
+      
+      
+    </>
+
+}
+
+export default Home;
